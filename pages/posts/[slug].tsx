a1b05011ed103fac50dda83737a3e257a06db5a6
Shin-sibainu <shincode0712@gmail.com>
2023-03-12 15:00:41 +0900

    mod

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -44,7 +44,6 @@ const Post = ({ post }) => {
       ))}
       <div className="mt-10 font-medium">
         <ReactMarkdown
-          children={post.markdown}
           components={{
             code({ node, inline, className, children }) {
               const match = /language-(\w+)/.exec(className || "");
@@ -60,7 +59,9 @@ const Post = ({ post }) => {
               );
             },
           }}
-        ></ReactMarkdown>
+        >
+          {post.markdown}
+        </ReactMarkdown>
 
         <Link href="/">
           <span className="pb-20 block mt-3 text-sky-900">←ホームに戻る</span>
